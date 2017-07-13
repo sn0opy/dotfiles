6282ca90a13540833fecc4dc09ad3069ad94a69f
Sascha Ohms <sasch9r@gmail.com>
2017-07-13 14:56:42 +0200

    add extra keybindings

--- .zsh/binds.zsh
+++ .zsh/binds.zsh
@@ -1,5 +1,6 @@
 bindkey "${terminfo[khome]}" beginning-of-line
 bindkey "${terminfo[kend]}" end-of-line
+bindkey "^[[3~" delete-char
 
 # fallback for macOS / iterm
 bindkey "^[[H" beginning-of-line
