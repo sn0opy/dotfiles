ad3cd0034cb7917d13872d8f30ebf29a96951a23
Sascha Ohms <sasch9r@gmail.com>
2018-08-27 10:50:20 +0200

    new paths for macos

--- .zsh/osx.zsh
+++ .zsh/osx.zsh
@@ -1,9 +1,10 @@
 if [[ "$OSTYPE" == darwin* ]]; then
 	export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
 
-	export PATH=$PATH:$HOME/composer/vendor/bin
+	export PATH=$PATH:$HOME/.composer/vendor/bin
 	export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH
 	export PATH=$PATH:/usr/local/opt/ruby/bin
+	export PATH=$PATH:$HOME/go/bin
 
 	export LC_ALL="en_US.UTF-8"
 	export EDITOR="vim"
@@ -13,4 +14,8 @@ if [[ "$OSTYPE" == darwin* ]]; then
 	alias ls="ls -G"
 	alias lt="ls -tcr"
 	alias llt="l -tcr"
+
+	if [ $commands[kubectl] ]; then
+		source <(kubectl completion zsh)
+	fi
 fi
