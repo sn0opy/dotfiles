b3d37922af19e86577ec8dfdcc93fdcce24aee28
Sascha Ohms <sasch9r@gmail.com>
2018-08-27 10:50:09 +0200

    new bindings

--- .zsh/binds.zsh
+++ .zsh/binds.zsh
@@ -2,6 +2,10 @@ bindkey "${terminfo[khome]}" beginning-of-line
 bindkey "${terminfo[kend]}" end-of-line
 bindkey "^[[3~" delete-char
 
+# alt + left/right
+bindkey '^[[1;9D' backward-word
+bindkey '^[[1;9C' forward-word
+
 # fallback for macOS / iterm
 bindkey "^[[H" beginning-of-line
 bindkey "^[[F" end-of-line
