275b68962d7cb34c19a640f7b8d0750ff2045e8b
Sascha Ohms <sasch9r@gmail.com>
2020-03-09 11:13:18 +0100

    feat: add kubectl alias

--- .zsh/macos.zsh
+++ .zsh/macos.zsh
@@ -1,23 +1,25 @@
 if [[ "$OSTYPE" == darwin* ]]; then
-	export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
+  export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
 
-	export PATH=$HOME/.composer/vendor/bin:$PATH
-	export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH
-	export PATH=$HOME/go/bin:$PATH
-	export PATH=$HOME/.fastlane/bin:$PATH
-	export PATH=/usr/local/opt/ruby/bin:$PATH
+  export PATH=$HOME/.composer/vendor/bin:$PATH
+  export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH
+  export PATH=$HOME/go/bin:$PATH
+  export PATH=$HOME/.fastlane/bin:$PATH
+  export PATH=/usr/local/opt/ruby/bin:$PATH
 
-	export LC_ALL="en_US.UTF-8"
-	export EDITOR="vim"
-	export VISUAL="vim"
-	export HOMEBREW_NO_ANALYTICS=1
-	export JAVA_HOME=`/usr/libexec/java_home`
+  export LC_ALL="en_US.UTF-8"
+  export EDITOR="vim"
+  export VISUAL="vim"
+  export HOMEBREW_NO_ANALYTICS=1
+  export JAVA_HOME=`/usr/libexec/java_home`
 
-	alias ls="ls -G"
-	alias lt="ls -tcr"
-	alias llt="l -tcr"
+  alias ls="ls -G"
+  alias lt="ls -tcr"
+  alias llt="l -tcr"
 
-	if [ $commands[kubectl] ]; then
-		source <(kubectl completion zsh)
-	fi
+  alias k="kubectl"
+
+  if [ $commands[kubectl] ]; then
+    source <(kubectl completion zsh)
+  fi
 fi
